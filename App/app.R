865ea375152e4af2580433d1708f29918dcbd207
SValv <s.valverde146@googlemail.com>
2021-04-03 14:22:56 +0200

    n2

--- App/app.R
+++ App/app.R
@@ -4,9 +4,22 @@
 library(tidyverse)
 library(DT)
 library(shiny)
+library(stringr)
 
 DF= readxl::read_excel("./Data/Pflanzenfile.xlsx")
 DF=DF[c(-1,-nrow(DF)),]
+a=DF[,30:33]
+
+b=data.frame(x=c(1:nrow(a)))
+for (i in 1:ncol(a)){
+    Effekt= str_extract(unlist(a[,i]),"[0-9]+")
+    Wirkung= str_trim(str_remove(unlist(a[,i]),"[0-9]+"))
+    b[paste("Wirkung", i, sep=" ")]=Wirkung
+    b[paste("W", i," Effekt",sep="")]=Effekt
+}
+
+DF=cbind(DF[,1:29],b[,-1])
+rm(a,b)
 
 color<-rep("#F3EACB",length(DF$Name))
 # Define UI for application that draws a histogram
@@ -21,7 +34,7 @@ ui <- fluidPage(
     )
 )
 
-Colls<-c("Name", "Kosten", "Häufigkeit", "Wirkung 1", "W1 Effekt", "Wirkung 2", "W2 Effekt", "Wirkung 3", "W3 Effekt", "Wirkung 4", "W4 Effekt")
+Colls<-c("Name", "Kosten", "HÃ¤ufigkeit", "Wirkung 1", "W1 Effekt", "Wirkung 2", "W2 Effekt", "Wirkung 3", "W3 Effekt", "Wirkung 4", "W4 Effekt")
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
