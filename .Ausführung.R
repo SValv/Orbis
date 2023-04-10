dc4ae66ddaee2b8a51634a9cf1a1297090f57c6b
Simon Valverde <59534036+SValv@users.noreply.github.com>
2023-04-11 01:34:49 +0200

    3.65

---
+++ .Ausführung.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(zip)
+
+dropboxpfad="E:/Dropbox/OrbisAsteaDropbox/"
+projektepfad = str_remove(getwd(),"###Orbis")
+
+
+# Copy and get files ------------------------------------------------------
+
+## First Copy the Rmd files from Dropbox to Disk
+
+file.copy(list.files(paste0(dropboxpfad,"Orbis Texte"), ".Rmd", full.names = T),getwd(),recursive = T)
+
+source(paste0(projektepfad,"SmallCodes/KeyIndexErstellen.R")) # Then make The KeyIndex File
+
+pfad=str_remove(getwd(),"###Orbis")
+
+## Holen der Downloadsheets
+file.copy(paste0(dropboxpfad,"Orbis Daten Tools/",c("CCSheetBlank.xlsx","CCrand.xlsx", "Einstieg in Orbis.pdf")),
+          "DownloadContainer",overwrite =T)
+
+## Übernehmen des Containers
+from <- paste0(dropboxpfad,"Orbis Daten Tools/Container/Data")
+file.copy(list.files(from, full.names = TRUE),
+          "Container/Data",
+          recursive = TRUE,overwrite =T)
+
+zip(
+  "DownloadContainer/OrbisAstea.zip",
+  c(paste0(projektepfad,"OrbisCodeBase/OrbisGui/auto-py-to-exe-master/output/Orbis Launcher")),
+  recurse = TRUE,
+  compression_level = 9,
+  include_directories = TRUE,
+  root = ".",
+  mode = c("cherry-pick")
+)
+
+from <- "DownloadContainer"
+to   <- "docs/DownloadContainer"
+file.copy(list.files(from, full.names = TRUE),
+          to,
+          recursive = TRUE,overwrite =T)
+
+## Kopieren der Bilder
+
+file.copy(paste0(dropboxpfad,"Orbis Pictures/Pictures/Maps/OrbisAstea.jpg"),
+          "docs",
+          recursive = TRUE,overwrite =T)
+
+file.copy(list.files(paste0(dropboxpfad,"Orbis Pictures/Pictures"),
+                     full.names = T),
+          "./Pictures",
+          recursive = TRUE)
+
+# Render Bookdown ---------------------------------------------------------
+
+bookdown::render_book(output_dir = "docs")
+rm(list = ls())
